973e9e02c3945940841793efba398144e4c6ddd0
Berkant Gunes <gunesberkant96@gmail.com>
2025-08-09 11:59:40 +0300

    Add files via upload

---
+++ HarvestController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agriculture.Controllers
+{
+    public class HarvestController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public HarvestController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult SendMessage()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult SendMessage(Contact contact)
+        {
+            contact.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            _contactService.Insert(contact);
+            return RedirectToAction("Index", "Harvest");
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+    }
+}
