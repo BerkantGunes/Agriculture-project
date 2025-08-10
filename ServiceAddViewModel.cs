9605806d72c46c3831ab6f3e1c168fac75743635
Berkant Gunes <gunesberkant96@gmail.com>
2025-08-10 13:50:49 +0300

    Add files via upload

---
+++ ServiceAddViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agriculture.Models
+{
+    public class ServiceAddViewModel
+    {
+        [Display(Name ="Title")]
+        [Required(ErrorMessage ="Title cannot be null!")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description cannot be null!")]
+        public string Description { get; set; }
+
+        [Display(Name = "Image")]
+        [Required(ErrorMessage = "Image cannot be null!")]
+        public string Image { get; set; }
+    }
+}
