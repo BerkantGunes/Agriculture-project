1694c6c3dcc4f699174e2f145ac15fa4d77f6176
Berkant Gunes <gunesberkant96@gmail.com>
2025-08-11 12:59:59 +0300

    Add files via upload

---
+++ _DashboardChartPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Agriculture.ViewComponents
+{
+    public class _DashboardChartPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = 82;
+            ViewBag.v2 = 96;
+            return View();
+        }
+    }
+}
